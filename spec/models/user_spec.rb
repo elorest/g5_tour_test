418b904cab3b1fc453c2c7ab3b42e4b652aa79aa
Isaac Sloan <isaac@Isaac-Sloans-MacBook-Pro.local>
2012-08-10 15:10:25 -0600

    test fixed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,6 @@ describe User do
   it "should create verification code" do
   	user = User.new(email: "isaac@isaacsloan.com", ip_address: "127.0.0.1")
   	user.save
-  	user.verification.should_not be_blank?
+  	user.verification.should_not be_empty
   end
 end
